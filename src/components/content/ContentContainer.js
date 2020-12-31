40351eabd48951e508718f9e766b213221367caf
Shivang Dave <daveshivang15@gmail.com>
2020-12-31 14:02:24 -0600

    Add more changes..

---
+++ src/components/content/ContentContainer.js
@@ -0,0 +1,14 @@
+import SCAbout from './SCAbout'
+import DCMain from './DCMain'
+
+const ContentContainer = () => {
+  return (
+    <div className="parent-overlay">
+      <SCAbout />
+      <DCMain />
+    </div>
+  )
+}
+
+
+export default ContentContainer;
