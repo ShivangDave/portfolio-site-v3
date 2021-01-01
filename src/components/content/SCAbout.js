4504a8718ed871bb17692b3250e593c382752511
Shivang Dave <daveshivang15@gmail.com>
2021-01-01 11:14:53 -0600

    Add social bar icons and links..

--- src/components/content/SCAbout.js
+++ src/components/content/SCAbout.js
@@ -1,4 +1,8 @@
 import github from '../../styles/imgs/github-144.png';
+import medium from '../../styles/imgs/medium-144.png';
+import music from '../../styles/imgs/music-100.png';
+import twitter from '../../styles/imgs/twitter-144.png';
+import linkedin from '../../styles/imgs/linkedin-144.png';
 
 const SCAbout = () => {
   return (
@@ -11,20 +15,24 @@ const SCAbout = () => {
 
       <span className="sc-social">
 
-        <a href={'https://google.com'} target={'_blank'} className="sc-social-btns">
+        <a href={'https://github.com/shivangdave'} target={'_blank'} rel="noreferrer" className="sc-social-btns">
           <img src={github} alt={''} />
         </a>
 
-        <a href={'https://google.com'} target={'_blank'} className="sc-social-btns">
-          <img src={github} alt={''} />
+        <a href={'https://shivang-dave.medium.com/'} target={'_blank'} rel="noreferrer" className="sc-social-btns">
+          <img src={medium} alt={''} />
         </a>
 
-        <a href={'https://google.com'} target={'_blank'} className="sc-social-btns">
-          <img src={github} alt={''} />
+        <a href={'https://music.apple.com/profile/shivangdave'} target={'_blank'} rel="noreferrer" className="sc-social-btns">
+          <img src={music} alt={''} />
         </a>
 
-        <a href={'https://google.com'} target={'_blank'} className="sc-social-btns">
-          <img src={github} alt={''} />
+        <a href={'https://twitter.com/@___Shivang___'} target={'_blank'} rel="noreferrer" className="sc-social-btns">
+          <img src={twitter} alt={''} />
+        </a>
+
+        <a href={'https://linkedin.com/in/dshivang'} target={'_blank'} rel="noreferrer" className="sc-social-btns">
+          <img src={linkedin} alt={''} />
         </a>
 
       </span>
